923a608db2005c4e9c783a71442c53a2e680efaf
robert-upwork <robyop.eyeco@gmail.com>
2020-12-29 09:24:27 +0700

    fix package.json sessions nav order

--- src/components/LiveStream/Banner/SessionNav/SessionNav.js
+++ src/components/LiveStream/Banner/SessionNav/SessionNav.js
@@ -1,23 +1,15 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import DateLink from './../DateLink/DateLink';
-import moment from 'moment';
 
 export default function SessionNav({ sessionDays }) {
-    const compareDates = (a, b) => {
-        const sessionDate1 = moment(a.date_string);
-        const sessionDate2 = moment(b.date_string);
-
-        return sessionDate1.isAfter(sessionDate2);
-    };
     return (
         <div>
             <div className="dates">
                 <ul>
-                    {sessionDays.sort(compareDates) &&
-                        sessionDays.map((item, index) => (
-                            <DateLink key={index} singleDay={item} active={true} />
-                        ))}
+                    {sessionDays.map((item, index) => (
+                        <DateLink key={index} singleDay={item} active={true} />
+                    ))}
                 </ul>
             </div>
         </div>
